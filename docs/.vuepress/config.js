3d96a54208a83d67ac0ad519be1a43999e6b57a8
didi <1191340528@qq.com>
2020-07-19 11:47:35 +0800

    feat: init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: 'kever.js',
+  description: 'nodejs framework',
+  themeConfig: {
+    logo: '/assets/img/logo.png',
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'API', link: '/api/' },
+      { text: 'GitHub', link: 'https://github.com/keverjs' },
+    ],
+    sidebar: 'auto',
+    lastUpdated: 'Last Updated',
+    nextLinks: false,
+    prevLinks: false,
+    smoothScroll: true,
+  },
+}
