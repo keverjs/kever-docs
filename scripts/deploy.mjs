e1f9e6b7b20204357b3562d74ae1937b3507964b
hubvue <1191340528@qq.com>
2021-05-17 19:13:13 +0800

    feat: shell -> $

--- scripts/deploy.mjs
+++ scripts/deploy.mjs
@@ -3,8 +3,8 @@
 import { $, cd} from 'zx'
 $.verbose = false
 
-errHandlerInit()
-function errHandlerInit() {
+initErrHandler()
+function initErrHandler() {
   process.on('unhandledRejection', (err, promise) => {
     console.log('err: ', err.toString())
   })
@@ -13,34 +13,29 @@ function errHandlerInit() {
   })
 }
 
-async function shell(command) {
-  const commandRes = await $(command)
-  return commandRes.toString().trim()
-}
-
 async function log(message) {
-  const now = await shell`date '+%Y-%m-%d %H:%M:%S'`
-  console.log(`[${now}]===========${message}============`)
+  const now = await $`date '+%Y-%m-%d %H:%M:%S'`
+  console.log(`[${now.toString().trim()}]===========${message.toString().trim()}============`)
 }
 
 log('env')
-log(`node version ${await shell`node -v`}`)
-log(`npm version ${await shell`npm -v`}`)
+log(`node version ${await $`node -v`}`)
+log(`npm version ${await $`npm -v`}`)
 
 log('npm install')
-await shell`npm install`
+await $`npm install`
 
 log('build vuepress')
-await shell`npm run build`
+await $`npm run build`
 
 cd('./docs/.vuepress/dist')
 
-await shell`rm -rf .git`
+await $`rm -rf .git`
 
-await shell`git init`
-await shell`git add -A`
-await shell`git commit -m'deploy'`
+await $`git init`
+await $`git add -A`
+await $`git commit -m'deploy'`
 
 log('push repos')
-await shell`git push -f git@github.com:keverjs/kever-docs.git master:gh-pages`
+await $`git push -f git@github.com:keverjs/kever-docs.git master:gh-pages`
 log('job done')
